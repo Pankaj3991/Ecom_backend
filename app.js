d871ce44acbb65c59e7922b5aeca96a8576aabdc
Pankaj3991 <pankajbansal3991@gmail.com>
2025-09-23 20:27:21 +0530

    added cors

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app = express();
 const errorMiddleware = require("./middleware/error.js");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({extended:true}));
+
+app.use(cors());
 
 // Routes imports -- 
 const userRoute = require("./routes/userRoute.js");
