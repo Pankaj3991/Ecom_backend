d679d59987efa458b0d79b118c0887622cfa9a51
Pankaj3991 <pankajbansal3991@gmail.com>
2025-10-07 10:21:13 +0530

    solved cors

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({extended:true}));
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: true, // Allow all origins for debugging
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 // Routes imports -- 
 const userRoute = require("./routes/userRoute.js");
